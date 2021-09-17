393fe77f73267d00f83d5e21479ec84cd4849528
Thomas Stephens <tmsteph1290@gmail.com>
2021-09-16 18:21:04 -0700

    committing

--- 08-React-Portfolio/my-portfolio/src/index.js
+++ 08-React-Portfolio/my-portfolio/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from "c:/Users/thoma/react-portfolio-demo/08-React-Portfolio/my-portfolio/src/app";
 
 ReactDOM.render(
   <React.StrictMode>
